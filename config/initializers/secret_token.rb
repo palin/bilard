40b0a279bef11879dc356b69cdbc6536f55b5fd7
Maciej Paliński <maciej.palinski@netguru.pl>
2012-02-16 13:58:09 +0100

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bilard::Application.config.secret_token = '76f0146802d623f6e4432e52f0aa53f141741fdb1c99ce39415540d45e755de736bb55ad8f174dc77965bdbf8cbde63f1442e5c5021f93f6775004ce7fd12693'
